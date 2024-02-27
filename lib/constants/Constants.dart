df6a304b608ceca5ec92a5bebbee44954fa8111e
Hossam Rakha <hossamrakha0@gmail.com>
2024-02-27 21:35:48 +0200

    created constants folder & init channel names

---
+++ lib/constants/Constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const String channelName = 'flutter_twitter_auth';
+  static const String channelEventName = 'flutter_twitter_auth_event';
+}
--- lib/flutter_twitter_auth.dart
+++ lib/flutter_twitter_auth.dart
@@ -1,7 +1,21 @@
-
 import 'flutter_twitter_auth_platform_interface.dart';
 
 class FlutterTwitterAuth {
+  /**
+   * The API key used to authenticate the call
+   */
+  final String? apiKey;
+  final String? apiSecretKey;
+  final String? redirectUri;
+
+  FlutterTwitterAuth(
+      {required this.apiKey,
+      required this.apiSecretKey,
+      required this.redirectUri});
+
+
+
+
   Future<String?> getPlatformVersion() {
     return FlutterTwitterAuthPlatform.instance.getPlatformVersion();
   }
